f4e8776df79624146fc1ef546b1f72dab25d59bf
Raajheer1 <raaj.patel229@gmail.com>
2022-08-10 16:45:06 -0500

    Updated urls for images

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ const app = express();
 app.use(express.json());
 
 let port = process.env.PORT || 3000;
-
-// make express look in the public directory for assets (css/js/img)
 app.use('/images', express.static(__dirname + '/multer/images/'));
 
 
